f97aa18945ae3b1b09aa35ce3820281e890c5be9
thecleancodellc-prog <thecleancodellc@gmail.com>
2025-10-06 22:06:26 -0400

    Remove experimental flag and add app/icon.png

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { serverActions: true },
+
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**' }
